773314dc75a80d9cb9d549f7e055935e23dad688
François Costa <frans1998@gmail.com>
2025-02-03 23:41:11 +0100

    add db url for test

--- fast_api/config.py
+++ fast_api/config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 API_KEY = os.getenv("MYSECRET_API_KEY")
